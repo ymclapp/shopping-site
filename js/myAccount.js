76c355e7311c6a7fa6c745bf3d84ab80be5189d7
ymclapp <ymclapp@yahoo.com>
2020-12-14 17:59:12 -0600

    cleaned up some of the unused files, added some function for logging in and a picture slideshow

--- js/myAccount.js
+++ js/myAccount.js
@@ -2,3 +2,12 @@
 
 // TO DO:
     // - Form submssion to save info
+    // - Login form
+
+    function login () {
+        let userid = prompt('Please enter your userid');
+        let pw = prompt('Please enter your password');
+    };
+
+    login();
+    
